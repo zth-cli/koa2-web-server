8dc4d08991093cc8f3db0f648bbb32cbbde91b26
阮志雄 <770304867@qq.com>
2021-04-27 01:04:21 +0800

    登录

--- controller/login.js
+++ controller/login.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 阮志雄
  * @Date: 2021-04-10 23:09:21
- * @LastEditTime: 2021-04-14 09:48:41
+ * @LastEditTime: 2021-04-27 01:01:11
  * @LastEditors: 阮志雄
  * @Description: 用户登录注册相关逻辑
  * @FilePath: \koa2-blog\controller\login.js
@@ -13,11 +13,12 @@ const LoginService = require('../service/login');
 class LoginController {
     static async signIn(ctx) {
         console.log(ctx.request.body);
-        let user = { name: ctx.request.body.userName, pass: ctx.request.body.pass }
+        let user = { name: ctx.request.body.username, pass: ctx.request.body.password }
         let data = await LoginService.findUserBypass(user)
+       
         if (data.length > 0) {
             const token = jwt.sign({ name: data[0].name, id: data[0].id }, config.jwtsign, { expiresIn: '2h' });
-            ctx.cookies.set('token', token)
+            ctx.cookies.set('token', {token, username:user.name})
             ctx.success(ctx, { token })
         } else {
             ctx.fail(ctx, '登录失败,检查用户名与密码是否正确')
