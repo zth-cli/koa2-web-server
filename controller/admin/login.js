b48e3b89268a9d0366089988c3425593197621cf
阮志雄 <770304867@qq.com>
2021-05-14 11:49:40 +0800

    文件上传优化

--- controller/admin/login.js
+++ controller/admin/login.js
@@ -1,17 +1,17 @@
 /*
  * @Author: 阮志雄
  * @Date: 2021-04-10 23:09:21
- * @LastEditTime: 2021-05-13 11:03:51
+ * @LastEditTime: 2021-05-14 11:42:34
  * @LastEditors: rzx007
  * @Description: 用户登录注册相关逻辑
  * @FilePath: \koa2-blog\controller\admin\login.js
  */
-const jwt = require("jsonwebtoken");
-const moment = require("moment");
-const fileUpLoad = require("../../utils/upload");
-const config = require("../../config/default");
-const LoginService = require("../../service/login");
-const { uuid } = require("../../utils/helper");
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const fileUpLoad = require('../../utils/upload');
+const config = require('../../config/default');
+const LoginService = require('../../service/login');
+const { uuid } = require('../../utils/helper');
 class LoginController {
   static async signIn(ctx) {
     console.log(ctx.request.body);
@@ -22,29 +22,27 @@ class LoginController {
     let data = await LoginService.findUserBypass(user);
 
     if (data.length > 0) {
-      const token = jwt.sign(
-        { name: data[0].name, id: data[0].id },
-        config.jwtsign,
-        { expiresIn: "2h" }
-      );
-      ctx.cookies.set("token", { token, username: user.name });
+      const token = jwt.sign({ name: data[0].name, id: data[0].id }, config.jwtsign, {
+        expiresIn: '2h',
+      });
+      ctx.cookies.set('token', { token, username: user.name });
       ctx.success(ctx, { token });
     } else {
-      ctx.fail(ctx, "登录失败,检查用户名与密码是否正确");
+      ctx.fail(ctx, '登录失败,检查用户名与密码是否正确');
     }
   }
   static async signUp(ctx) {
-    const time = moment().format("YYYY-MM-DD hh:mm:ss");
-    let obj = [uuid(), "rzx100", "123456", "", time, time, "1"];
-    // 注意 ctx.request.files 中的 files 与前端页面传入的字段相对应起来
-    if (ctx.request.files) {
-      const files = ctx.request.files.files;
+    const time = moment().format('YYYY-MM-DD hh:mm:ss');
+    let obj = [uuid(), 'rzx100', '123456', '', time, time, '1'];
+    console.log(ctx.request.files);
+    if (Object.keys(ctx.request.files).length > 0) {
+      const files = ctx.request.files;
       let pathArr = await fileUpLoad(files);
       obj[3] = pathArr[0];
       let data = await LoginService.signUp(ctx, obj);
-      data&& ctx.success(ctx, pathArr[0]);
+      data && ctx.success(ctx, pathArr);
     } else {
-      ctx.fail(ctx, "无上传文件");
+      ctx.fail(ctx, '无上传文件');
     }
   }
 }
--- utils/upload.js
+++ utils/upload.js
@@ -1,14 +1,14 @@
 /*
  * @Author: 阮志雄
  * @Date: 2021-04-11 15:09:56
- * @LastEditTime: 2021-05-13 10:33:19
+ * @LastEditTime: 2021-05-14 11:40:40
  * @LastEditors: rzx007
  * @Description: 文件上传逻辑抽象,支持单文件和多文件上传
  * @FilePath: \koa2-blog\utils\upload.js
  */
 
 const path = require('path');
-const fs = require('fs');
+const fs = require('mz/fs');
 module.exports = async (files, temp) => {
   let basePath = path.join(__dirname, '..\\public\\')
   let tempPath = temp || 'file\\'
@@ -20,19 +20,15 @@ module.exports = async (files, temp) => {
       } 
     });
   }
-  if (files && files.length === undefined) { // 单文件上传
-    let newFilename = await upload(files, basePath, tempPath)
+  for (let key in files) {
+    let newFilename = await upload(files[key], basePath, tempPath)
     filepathArr.push(newFilename)
-  } else {
-    for (let file of files) {
-      let newFilename = await upload(file, basePath, tempPath)
-      filepathArr.push(newFilename)
-    }
   }
   return filepathArr
 }
 // 创建文件流，保存文件
 async function upload(file, basePath, tempPath) {
+  try {
   const reader = fs.createReadStream(file.path)  // 创建可写流
   // 对文件重命名，以防名称冲突
   const newFilename = tempPath + Math.random(10).toString(36) + new Date().getTime() + '.' + file.name.split('.').pop();
@@ -41,6 +37,12 @@ async function upload(file, basePath, tempPath) {
   const upStream = fs.createWriteStream(newFilepath);
   reader.pipe(upStream);
   return newFilename
+  } finally {
+    // 需要删除临时文件
+    console.log('-----12212----');
+    await fs.unlink(file.path);
+  }
+  
 }
 // https://www.jianshu.com/p/34d0e1a5ac70 文件上传
 
